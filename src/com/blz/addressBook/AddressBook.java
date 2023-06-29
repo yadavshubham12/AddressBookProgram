9e7e78150da450c9833f3a0aa600cb002cf84dc6
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-06-29 23:36:25 +0530

    ADDED: Created Contact Details

--- src/com/blz/addressBook/AddressBook.java
+++ src/com/blz/addressBook/AddressBook.java
@@ -1,7 +1,90 @@
 package com.blz.addressBook;
 
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
+
+        String firstName;
+        String lastName;
+        String address;
+        String city;
+        String state;
+        double zip;
+        long phoneNumber;
+        long email;
+
+    public AddressBook(String firstName, String lastName, String address, String city, String state, double zip, long phoneNumber, long email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getZip() {
+        return zip;
+    }
+
+    public void setZip(double zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getEmail() {
+        return email;
+    }
+
+    public void setEmail(long email) {
+        this.email = email;
     }
 }
+
+
---
+++ src/com/blz/addressBook/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.blz.addressBook;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+
+    }
+}
